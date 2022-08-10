e34313108424f40e45ec3287a100a51a4c9331a4
David MacMahon <davidm@astro.berkeley.edu>
2022-08-11 01:13:36 +0200

    Make DirWatcher robust against race condition
    When creating multiple levels of subdirectories in a watched directory,
    it is possible for the second level subdirectory to be created before
    DirWatcher has added a watch to the first level subdirectory.  Prior to
    this commit, DirWatcher would be oblivious to the second level
    subdirectory so any/all activity there would be missed.  To avoid this
    predicament, whenever DirWatcher adds a watch to a newly created
    directory it walks the contents of that newly created directory to
    ensure that it will detect any subdirectories or files that were created
    before the watch was added.  It is possible that this will detetct files
    created immediately after the watch was created which will also be
    detected by the watch, so clients should be prepared to handle duplicate
    creation events for a given entity.

--- src/dirwatcher.jl
+++ src/dirwatcher.jl
@@ -1,7 +1,8 @@
 struct DirWatcher
     fd::RawFD
     mask::UInt32
-    watches::Dict{Int32, String}
+    watches::Dict{Int32, String} # watch_descriptor=>name
+    dirs::Dict{String, Int32} # absdirname=>watch_descritor
     run::Ref{Bool}
     task::Ref{Task}
 
@@ -12,15 +13,14 @@ struct DirWatcher
         fd = inotify_open()
 
         watches = Dict{Int32, String}()
-        for (root, dirs, files) in walkdir(dir, topdown=false)
-            @debug "adding watch for $root mask $mask"
-            wd = inotify_add_watch(fd, root, mask)
-            watches[wd] = root
-        end
-
+        dirs = Dict{String, Int32}()
         run = Ref(true)
 
-        dirwatcher = new(fd, mask, watches, run, Ref{Task}())
+        dirwatcher = new(fd, mask, watches, dirs, run, Ref{Task}())
+
+        # Use add_watch() to add dir and all subdirs recursively
+        add_watch(f, dirwatcher, dir; kwargs...)
+
         dirwatcher.task[] = @async begin
             @info "watcher task for $fd starting"
             try
@@ -49,11 +49,55 @@ function DirWatcher(channel::AbstractChannel, dir, mask=ALL_EVENTS)
     DirWatcher((den)->put!(channel, den), dir, mask)
 end
 
+"""
+    add_watch(f, dirwatcher::DirWatcher, dir::AbstractString; kwargs...)
+Add an `inotify` watch on `dir` to `dirwatcher`.  `f` is passed a
+`DirEventNameTuple` for `dir` and every file and subdir that exists in/under
+`dir` after the watch is added.  The `event` field of the passed
+`DirEventNameTuple` instances will be `CREATE_DIR` for directories and `CREATE`
+for files.
+"""
+function add_watch(f, dirwatcher::DirWatcher, dir::AbstractString; kwargs...)
+    @debug "add_watch" dirwatcher.fd dir
+
+    # If we are already watching subdir then return
+    haskey(dirwatcher.dirs, dir) && return
+
+    # Call inotify_add_watch for this dirwatcher and dir
+    @debug "add_watch calling inotify_add_watch" dirwatcher.fd dir dirwatcher.mask
+    wd = inotify_add_watch(dirwatcher.fd, dir, dirwatcher.mask)
+    dirwatcher.watches[wd] = dir
+    dirwatcher.dirs[dir] = wd
+
+    @debug "calling DirWatcher client function" dir
+    f((dir=dirname(dir), event=CREATE_DIR, name=basename(dir)); kwargs...)
+    @debug "back from DirWatcher client function"
+
+    # Now that we've called inotify_add_watch, handle existing content (either
+    # pre-existing or newly created but prior to adding the watch)
+
+    # Call client function for all files in `dir`
+    for file in readdir(dir; join=true)
+        isfile(file) || continue
+        @debug "calling DirWatcher client function" file
+        f((dir=dirname(file), event=CREATE, name=basename(file)); kwargs...)
+        @debug "back from DirWatcher client function"
+    end
+
+    # walk dir and add call add_watch recursively for all encountered
+    # directories.
+    for (subdir, _, _) in walkdir(dir)
+        subdir == dir && continue
+        add_watch(f, dirwatcher, subdir; kwargs...)
+    end
+end
+
 function watch_dir_loop(f::Function, dirwatcher::DirWatcher; kwargs...)
     buf=Array{UInt8}(undef, 4096)
     while dirwatcher.run[]
         @debug "watch_dir_loop for $fd calling inotify_read_events"
         inotify_read_events(dirwatcher.fd, buf) do (ev, name)
+            # Find directory corresponding to this event
             dir = get(dirwatcher.watches, ev.wd, nothing)
 
             # When a subdir is created in a watched directory, we have to call
@@ -61,22 +105,25 @@ function watch_dir_loop(f::Function, dirwatcher::DirWatcher; kwargs...)
             # the new watch descriptor to store the subdir name in
             # `dirwatcher.watches`.
             if iscreatedir(ev)
+                # Add watch for new dir to dirwatcher
                 subdir = joinpath(dir, name)
-                @debug "watch_dir_loop calling inotify_add_watch" dirwatcher.fd subdir dirwatcher.mask
-                wd = inotify_add_watch(dirwatcher.fd, subdir, dirwatcher.mask)
-                dirwatcher.watches[wd] = subdir
+                add_watch(f, dirwatcher, subdir; kwargs...)
+            else # otherwise just call `f`
+                @debug "calling DirWatcher client function"
+                f((dir=dir, event=ev, name=name); kwargs...)
+                @debug "back from DirWatcher client function"
             end
-
-            @debug "calling DirWatcher client function"
-            f((dir=dir, event=ev, name=name); kwargs...)
-            @debug "back from DirWatcher client function"
 
             # When a subdir is deleted, we do not have to call inotify_rm_watch
             # in response to DELETE_SELF because the watch is automatically
             # removed.  The automatic removal of the watch results in an IGNORED
             # event, which we use to delete the old watch descriptor from
             # `dirwatcher.watches`.
-            isignored(ev) && delete!(dirwatcher.watches, ev.wd)
+            if isignored(ev) && haskey(dirwatcher.watches, ev.wd)
+                # Delete dirwatcher.dirs entry
+                delete!(dirwatcher.dirs, dir)
+                delete!(dirwatcher.watches, ev.wd)
+            end
         end
         @debug "watch_dir_loop for $fd back from inotify_read_events"
     end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,34 +1,110 @@
 using INotify
 using Test
 
-@testset "INotify.jl" begin
+@testset "inotify" begin
     mktempdir() do dir
         inotify_open() do inotify
+            # Add watch
             wd = inotify_add_watch(inotify, dir, INotify.CREATE|INotify.DELETE)
+
+            # Add two levels of subdirectories
+            subdirpath = joinpath(dir, "subdir1", "subdir2")
+            mkpath(subdirpath)
+
+            # Read inotify events
+            evs = inotify_read_events(inotify)
+            @test_broken length(evs) == 2
+
+            # Validate events
+            for (i, (ev, name)) in enumerate(evs)
+                @debug "got event" ev
+                @test ev.mask == INotify.CREATE_DIR
+                @test name == "subdir$i"
+            end
+
+            # Add file
             fname = joinpath(dir, "foo")
             open(fname, "w") do f
                 print(f, "hello, world")
             end
 
+            # Read inotify events
             evs = inotify_read_events(inotify)
             @test length(evs) == 1
 
+            # Validate event
             ev, name = evs[1]
+            @debug "got event" ev
             @test ev.mask == INotify.CREATE
             @test name == "foo"
-            @show ev
 
+            # Remove file
             rm(fname)
 
+            # Read inotify events
             evs = inotify_read_events(inotify)
             @test length(evs) == 1
 
+            # Validate event
             ev, name = evs[1]
+            @debug "got event" ev
             @test ev.mask == INotify.DELETE
             @test name == "foo"
-            @show ev
 
-            @test inotify_rm_watch(inotify, wd) === nothing
+        end # inotify_open
+    end # mktempdir
+end # testset inotify
+
+@testset "dirwatcher" begin
+    mktempdir() do dir
+
+        evcount = Ref{Int}(0)
+
+        function incr(den; evc)
+            @debug "in incr" den evc
+            evc[] += 1
         end
-    end
-end
+
+        # Create DirWatcher for `dir`
+        dw = INotify.DirWatcher(incr, dir, INotify.CREATE|INotify.DELETE; evc=evcount)
+
+        # Add two levels of subdirectories
+        subdirpath = joinpath(dir, "subdir1", "subdir2")
+        mkpath(subdirpath)
+
+        # Verify that event counter got three events
+        sleep(2.0) # Give dw some time to work (a bit longer due to compilation)
+        @test evcount[] == 3
+
+        # Verify that dw is watching all three directories
+        expected = [dir, joinpath(dir, "subdir1"), joinpath(dir, "subdir1/subdir2")]
+        @test sort(collect(values(dw.watches))) == expected
+        @test sort(collect(keys(dw.dirs))) == expected
+
+        # Reset evcount
+        evcount[] = 0
+
+        # Add file
+        fname = joinpath(dir, "subdir1/subdir2/foo")
+        open(fname, "w") do f
+            print(f, "hello, world")
+        end
+
+        # Verify that event counter got 1 event
+        sleep(0.1) # Give dw some time to work
+        @test evcount[] == 1
+
+        # Reset evcount
+        evcount[] = 0
+
+        # Remove subdir1 recursively
+        rm(joinpath(dir, "subdir1"), recursive=true)
+
+        # Verify that event counter got 5 events (3 DELETEs + 2 IGNOREDs)
+        sleep(0.1) # Give dw some time to work
+        @test evcount[] == 5
+
+        close(dw)
+
+    end # mktempdir
+end # testset dirwatcher
